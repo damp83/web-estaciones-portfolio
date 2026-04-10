3c036c9b22ebd867e4b107fca243f771e6133ce7
damp83 <diegoalberto.moya@gmail.com>
2026-04-10 17:48:19 +0200

    style: apply premium typography Outfit and Nunito

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+        heading: ['Outfit', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
