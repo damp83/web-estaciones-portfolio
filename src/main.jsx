08e9cbaf658ffdad701d136dfcbd1192ad02e6c4
damp83 <diegoalberto.moya@gmail.com>
2026-04-12 20:59:54 +0200

    feat: Add Vercel Analytics and Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
